0a2edaa5635e3c8034c25ef3fa41f4b61bd0e0a8
Vivek Kadvani <vkkadvani8866@gmail.com>
2024-03-05 17:35:26 +0530

    first commit for merlin chain

---
+++ interface/src/constants/lists.ts
@@ -0,0 +1,8 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/VivekKadvani/tokenData/dev/OTPToken.json'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  // DEFAULT_TOKEN_LIST_URL,
+  // 'tokenlist.aave.eth',
+  'https://raw.githubusercontent.com/VivekKadvani/tokenData/dev/OTPToken.json'
+]
